b5fe8dd0d04b29a9d33405fea3b8acd3b2b10b68
Gian Ntzik <gian.ntzik08@imperial.ac.uk>
2014-04-17 01:24:20 +0100

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,13 @@
 if [ ! -f packages/FAKE/tools/FAKE.exe ]; then
   mono .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
 fi
+#workaround assembly resolution issues in build.fsx
 export FSHARPI=`which fsharpi`
-PATH="$PWD:$PATH"
+cat - > fsharpi <<"EOF"
+#!/bin/bash
+libdir=$PWD/packages/FAKE/tools/
+$FSHARPI --lib:$libdir $@
+EOF
+chmod +x fsharpi
 mono packages/FAKE/tools/FAKE.exe build.fsx $@
+rm fsharpi
