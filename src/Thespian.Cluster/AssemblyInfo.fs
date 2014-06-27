4c2f065052ab37a407bdb396ed905557b5ba9395
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2014-06-27 13:28:00 +0300

    version update

--- src/Thespian.Cluster/AssemblyInfo.fs
+++ src/Thespian.Cluster/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Thespian.Cluster")>]
 [<assembly: AssemblyProductAttribute("Thespian.Cluster")>]
 [<assembly: AssemblyDescriptionAttribute("An F# Actor Framework")>]
-[<assembly: AssemblyVersionAttribute("0.0.6")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.6")>]
+[<assembly: AssemblyVersionAttribute("0.0.7")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.7")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.6"
+    let [<Literal>] Version = "0.0.7"
