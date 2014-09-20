1257587489292b506c6176b66eb0d6a645ef7afe
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2014-09-20 21:55:19 +0300

    add xml comments to serialization.fs

--- src/Thespian/Serialization.fs
+++ src/Thespian/Serialization.fs
@@ -1,72 +1,69 @@
 ﻿module Nessos.Thespian.Serialization
 
-open System
-open System.Runtime.Serialization
+    open System
+    open System.Runtime.Serialization
 
-open Nessos.FsPickler
-open Nessos.Thespian
-open Nessos.Thespian.Utils.Concurrency
+    open Nessos.FsPickler
+    open Nessos.Thespian
 
-type IMessageSerializer =
-    abstract Name: string  
-    abstract Serialize<'T> : 'T * ?context:StreamingContext -> byte []
-    abstract Deserialize<'T> : data:byte[] * ?context:StreamingContext -> 'T
+    /// Abstract message serialization interface used by Thespian
+    type IMessageSerializer =
+        /// Serialization identifier
+        abstract Name: string  
 
-type FsPicklerMessageSerializer(?pickler : FsPicklerSerializer) =
-    let pickler =
-        match pickler with 
-        | None -> new BinarySerializer() :> FsPicklerSerializer
-        | Some p -> p
+        /// <summary>
+        ///     Serialize a value to byte array.
+        /// </summary>
+        /// <param name="T">serialized value.</param>
+        /// <param name="context">Streaming context.</param>
+        abstract Serialize<'T> : 'T * ?context:StreamingContext -> byte []
 
-    member __.Pickler = pickler
+        /// <summary>
+        ///     Deserialize a value of byte array.
+        /// </summary>
+        /// <param name="data">pickled value.</param>
+        /// <param name="context">Streaming context.</param>
+        abstract Deserialize<'T> : data:byte[] * ?context:StreamingContext -> 'T
 
-    member __.Serialize<'T>(value: 'T, ?context) = pickler.Pickle<'T>(value, ?streamingContext = context)
-    member __.Deserialize<'T>(data: byte[], ?context) = pickler.UnPickle<'T>(data, ?streamingContext = context)
+    /// <summary>
+    ///     FsPickler Implementation for IMessageSerializer.
+    /// </summary>
+    type FsPicklerMessageSerializer(?pickler : FsPicklerSerializer) =
 
-    interface IMessageSerializer with
-        override __.Name = sprintf "FsPickler.%s" pickler.PickleFormat
+        let pickler =
+            match pickler with 
+            | None -> new BinarySerializer() :> FsPicklerSerializer
+            | Some p -> p
 
-        override self.Serialize<'T> (value: 'T, ?context) =
-            try self.Serialize<'T>(value, ?context = context)
-            with e -> raise <| new ThespianSerializationException(sprintf "Failed to serialize value of type %A" typeof<'T>.Name, SerializationOperation.Serialization, e)
-        override self.Deserialize<'T>(data: byte[], ?context) =
-            try self.Deserialize<'T>(data, ?context = context)
-            with e -> raise <| new ThespianSerializationException(sprintf "Failed to deserialize value with expected type %A" typeof<'T>.Name, SerializationOperation.Deserialization, e)
+        /// <summary>
+        ///     Direct access to the FsPickler instance.
+        /// </summary>
+        member __.Pickler = pickler
+
+        /// <summary>
+        ///     Serialize a value to byte array.
+        /// </summary>
+        /// <param name="T">serialized value.</param>
+        /// <param name="context">Streaming context.</param>
+        member __.Serialize<'T>(value: 'T, ?context) = pickler.Pickle<'T>(value, ?streamingContext = context)
+
+        /// <summary>
+        ///     Deserialize a value of byte array.
+        /// </summary>
+        /// <param name="data">pickled value.</param>
+        /// <param name="context">Streaming context.</param>
+        member __.Deserialize<'T>(data: byte[], ?context) = pickler.UnPickle<'T>(data, ?streamingContext = context)
+
+        interface IMessageSerializer with
+            override __.Name = sprintf "FsPickler.%s" pickler.PickleFormat
+
+            override self.Serialize<'T> (value: 'T, ?context) =
+                try self.Serialize<'T>(value, ?context = context)
+                with e -> raise <| new ThespianSerializationException(sprintf "Failed to serialize value of type %A" typeof<'T>.Name, SerializationOperation.Serialization, e)
+            override self.Deserialize<'T>(data: byte[], ?context) =
+                try self.Deserialize<'T>(data, ?context = context)
+                with e -> raise <| new ThespianSerializationException(sprintf "Failed to deserialize value with expected type %A" typeof<'T>.Name, SerializationOperation.Deserialization, e)
 
 
-let mutable defaultSerializer = new FsPicklerMessageSerializer() :> IMessageSerializer
-
-        
-[<Obsolete("use defaultSerializer value instead.")>]
-type SerializerRegistry private () =
-    static let defaultSerializerName = String.Empty
-    static let originalDefaultSerializer = new FsPicklerMessageSerializer() :> IMessageSerializer
-    static let serializerMap = Atom.atom Map.empty<string, IMessageSerializer>
-    static let init () =
-        serializerMap.Swap(fun _ -> 
-            let map = Map.empty
-            let map = Map.add defaultSerializerName originalDefaultSerializer map
-            let map = Map.add originalDefaultSerializer.Name originalDefaultSerializer map
-            map)
-
-    static do init ()
-
-    static member Register(serializer: IMessageSerializer, ?setAsDefault: bool) =
-        serializerMap.Swap(fun m ->
-            let m = Map.add serializer.Name serializer m
-            if defaultArg setAsDefault false then
-                Map.add defaultSerializerName serializer m
-            else m)
-
-    static member DefaultName = serializerMap.Value.[defaultSerializerName].Name
-    static member GetDefaultSerializer() = serializerMap.Value.[defaultSerializerName]
-
-    static member SetDefault(name: string) =
-        serializerMap.Swap(fun m ->
-            match m.TryFind name with
-            | Some serializer -> Map.add defaultSerializerName serializer m
-            | None -> invalidArg "name" "No such serializer registered.")
-
-    static member Resolve(name: string) = serializerMap.Value.[name]
-    static member IsRegistered(name: string) = serializerMap.Value.ContainsKey name
-    static member Clear() = init ()
+    /// Gets or sets the default global serializer used by Thespian
+    let mutable defaultSerializer = new FsPicklerMessageSerializer() :> IMessageSerializer
