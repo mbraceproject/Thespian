7284e20d01e24e16200ae427ce96b637651bda1c
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2016-01-28 02:13:36 +0200

    bump version

--- src/Thespian/AssemblyInfo.fs
+++ src/Thespian/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Thespian")>]
 [<assembly: AssemblyProductAttribute("Thespian")>]
 [<assembly: AssemblyDescriptionAttribute("An F# Actor Framework")>]
-[<assembly: AssemblyVersionAttribute("0.1.9")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.9")>]
+[<assembly: AssemblyVersionAttribute("0.1.10")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.10")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.9"
+    let [<Literal>] Version = "0.1.10"
