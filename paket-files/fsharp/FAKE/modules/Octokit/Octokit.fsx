9aee185cbb2a69b6c77bf990046252c228c4babb
Don Syme <dsyme@microsoft.com>
2017-06-12 18:06:51 +0100

    updates

--- paket-files/fsharp/FAKE/modules/Octokit/Octokit.fsx
+++ paket-files/fsharp/FAKE/modules/Octokit/Octokit.fsx
@@ -28,7 +28,7 @@ type private HttpClientWithTimeout(timeout : TimeSpan) as this =
     let setter = lazy(
         match typeof<HttpClientAdapter>.GetField("_http", BindingFlags.NonPublic ||| BindingFlags.Instance) with
         | null -> ()
-        | f -> 
+        | f ->
             match f.GetValue(this) with
             | :? HttpClient as http -> http.Timeout <- timeout
             | _ -> ())
@@ -173,9 +173,9 @@ let getLastRelease owner project (client : Async<GitHubClient>) =
             DraftRelease = draft }
     }
 
-let getReleaseByTag owner project tag (client : Async<GitHubClient>) =
+let getReleaseByTag (owner:string) (project:string) (tag:string) (client : Async<GitHubClient>) =
     retryWithArg 5 client <| fun client' -> async {
-        let! drafts = Async.AwaitTask <| client'.Repository.Release.GetAll(owner, project)
+        let! drafts = client'.Repository.Release.GetAll(owner, project) |> Async.AwaitTask
         let matches = drafts |> Seq.filter (fun (r: Release) -> r.TagName = tag)
 
         if Seq.isEmpty matches then
